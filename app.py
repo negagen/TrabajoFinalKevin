bd30f89f1ffc1e282ff2d2f1f5b0ab6c9a50e9de
Brandom Vasquez <negagen@aol.com>
2020-08-22 16:52:25 -0500

    Ordenando codigo

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
-import re
+import re, math
 
 n = int(input("Ingrese el numero de vertices: "))
 
-angulos_observados = []
-suma_teorica = (n+2)*180
-suma_observable = 0
+angulo = dict()
 
-print("Ingrese el angulo externo en grados, minutos y segundos de cada vertice: ")
-for i in range(0,n+1):
-    dms_str = input( str(i+1) + ":" )
-    
+def decimal_a_gmd(angulo_decimal ):
+    grados, minutos = math.modf(angulo_decimal)
+    minutos, segundos = math.modf(minutos*60)
+    segundos = segundos*60
+    return (grados, minutos, segundos)
+
+
+def gmd_a_decimal(grados, minutos, segundos):
+    return grados + minutos / 60 + segundos / 3600
+
+
+def parse_gmd_texto(dms_str):
     numbers = [*filter(len, re.split('\D+', dms_str, maxsplit=4))]
 
     degree = numbers[0]
@@ -18,8 +24,16 @@ for i in range(0,n+1):
     frac_seconds = numbers[3] if len(numbers) >= 4 else '0'
     
     second += "." + frac_seconds
-    
-    angulo = int(degree) + float(minute) / 60 + float(second) / 3600
+    return (int(degree), float(minute), float(second))
+
+
+angulos_observados = []
+suma_teorica = (n+2)*180
+suma_observable = 0
+
+print("Ingrese el angulo externo en grados, minutos y segundos de cada vertice: ")
+for i in range(0,n+1):
+    angulo = gmd_a_decimal(*parse_gmd_texto(input( str(i+1) + ":" )))
     suma_observable += angulo if i>0 else 0
     print(suma_observable)
     angulos_observados.append(angulo)
