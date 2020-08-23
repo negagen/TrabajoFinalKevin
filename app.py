00edf22c3fc31fd9ce09e94b9765b58b25d3fd8a
Brandom Vasquez <negagen@aol.com>
2020-08-23 01:39:39 -0500

    Se imprimen los angulos corregidos

--- app.py
+++ app.py
@@ -83,7 +83,6 @@ print("Correcion angular: "+ str(round(correcion_angular,6)))
 angulos_corregidos = list(map(lambda angulo : angulo+correcion_angular, angulos_observados))
 
 angulos_azimut = list(obtener_angulos_azimut(angulos_corregidos,angulo_azimut_base))
-print(angulos_azimut)
 
 print("Angulos corregidos para cada vertice:")
 for grado, minuto, segundo in [decimal_a_gms(angulo) for angulo in angulos_corregidos]:
